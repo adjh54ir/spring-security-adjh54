b8487ab3dfaa930ddd6ac5aeb79003315cd1dfe7
Lee <adjh54ir@gmail.com>
2023-01-09 16:46:37 +0900

    init config

---
+++ src/main/java/com/adjh/sprsecurity/SpringSecurityAdjhApplication.java
@@ -0,0 +1,13 @@
+package com.adjh.sprsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAdjhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAdjhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adjh/sprsecurity/SpringSecurityAdjhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adjh.sprsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAdjhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
